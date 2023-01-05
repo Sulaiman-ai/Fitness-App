f7312e57dc8edd27bf1bc391fae9b9358ea0ed17
Zak khan <zakaria2012@gmail.com>
2023-01-05 15:42:20 +0000

    Jason clips outputed on to the page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,37 @@
 var apiKey = 'AIzaSyA7GFSrrG8v7j_vHqluZD_SnoUS-LgLiDE';
-var youtube = 'https://www.youtube.com/watch?'
+var youtube = 'https://www.youtube.com/embed/'
+//var chanelsID = 'golujalalabadvlogs571';
 
 
-$.get(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelType=any&type=video&type=channel&videoType=any&key=${apiKey}`)
+https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCsC9Y1KhOw-aKSUBE0F_ikQ&channelType=channelTypeUnspecified&videoType=videoTypeUnspecified&key=[YOUR_API_KEY]' \
+
+
+$.get(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCsC9Y1KhOw-aKSUBE0F_ikQ&channelType=channelTypeUnspecified&videoType=videoTypeUnspecified&key=${apiKey}`)
     .then(data => {
+        var domContainer = $('.cotainer');
+        console.log('second api', data);
 
-        console.log('second api', data)
 
-        var videoId = data.items[1].id.videoId;
-        var channelId = data.items[1].snippet.channelId;
-        var channelTitle = data.items[1].snippet.title;
-        var description = data.items[1].snippet.description;
 
-        console.log(`videoId: ${videoId} channelId: ${channelId} channelTitle: ${channelTitle} description: ${description}`)
+        data.items.forEach((el) => {
 
-        console.log(youtube + "v=" + videoId + "&ab_channel=" + channelId);
+            console.log(el);
+            var videoId = el.id.videoId;
+            var channelId = el.snippet.channelId;
+            var channelTitle = el.snippet.title;
+            var description = el.snippet.description;
+
+            console.log(`videoId: ${videoId} channelId: ${channelId} channelTitle: ${channelTitle} description: ${description}`)
+            console.log(youtube + "v=" + videoId + "&ab_channel=" + channelId);
+
+            var videos = `<iframe width="420" height="315" src='${youtube}${videoId}'></iframe>
+            
+                <p><h2>${channelTitle}</h2></p>
+                <p>${description}</p>`
+            domContainer.append(videos);
+        });
+
+
 
 
     });
