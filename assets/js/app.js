144be22d1679ed2445a4e049a495685045869ef8
Zak khan <zakaria2012@gmail.com>
2023-01-06 09:45:15 +0000

    refactored code and added few more channels to get the data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,37 +1,95 @@
 var apiKey = 'AIzaSyA7GFSrrG8v7j_vHqluZD_SnoUS-LgLiDE';
-var youtube = 'https://www.youtube.com/embed/'
-//var chanelsID = 'golujalalabadvlogs571';
+var youtube = 'https://www.youtube.com/embed/';
+var fetchApi = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet'
+var otherSearchParameters = 'channelType=channelTypeUnspecified&videoType=videoTypeUnspecified'
 
+var userChannelId = [
+    {
+        'channelId': 'channelId=UCsC9Y1KhOw-aKSUBE0F_ikQ'
+    },
+    {
+        'channelId': 'channelId=UCs_azWRsdYDhKonquMlAMfQ',
+    },
+    {
+        'channelId': 'channelId=UCjTp-nBKswYLumqmVeBPwYw'
+    }
+];
 
-https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCsC9Y1KhOw-aKSUBE0F_ikQ&channelType=channelTypeUnspecified&videoType=videoTypeUnspecified&key=[YOUR_API_KEY]' \
+async function fetchApiData(UserId) {
+    return $.get(`${fetchApi}&key=${apiKey}&${UserId}&${otherSearchParameters}`)
+        .then(resp => {
+            return resp;
+        });
+}
 
+async function goodVibesPodCast() {
+    var ipareturnedData = await fetchApiData(userChannelId[0].channelId);
+    console.log('getAPIData function:  ' + ipareturnedData);
+    var domContainer = $('.cotainer');
+    ipareturnedData.items.forEach((el) => {
+        console.log(el);
+        var videoId = el.id.videoId;
+        var channelId = el.snippet.channelId;
+        var channelTitle = el.snippet.title;
+        var description = el.snippet.description;
 
-$.get(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCsC9Y1KhOw-aKSUBE0F_ikQ&channelType=channelTypeUnspecified&videoType=videoTypeUnspecified&key=${apiKey}`)
-    .then(data => {
-        var domContainer = $('.cotainer');
-        console.log('second api', data);
+        // console.log(`videoId: ${videoId} channelId: ${channelId} channelTitle: ${channelTitle} description: ${description}`)
+        // console.log(youtube + "v=" + videoId + "&ab_channel=" + channelId);
 
-
-
-        data.items.forEach((el) => {
-
-            console.log(el);
-            var videoId = el.id.videoId;
-            var channelId = el.snippet.channelId;
-            var channelTitle = el.snippet.title;
-            var description = el.snippet.description;
-
-            console.log(`videoId: ${videoId} channelId: ${channelId} channelTitle: ${channelTitle} description: ${description}`)
-            console.log(youtube + "v=" + videoId + "&ab_channel=" + channelId);
-
-            var videos = `<iframe width="420" height="315" src='${youtube}${videoId}'></iframe>
-            
+        var videos = `<iframe width="420" height="315" src='${youtube}${videoId}'></iframe>
                 <p><h2>${channelTitle}</h2></p>
                 <p>${description}</p>`
-            domContainer.append(videos);
-        });
-
-
-
-
+        domContainer.append(videos);
     });
+}
+goodVibesPodCast();
+
+async function keepMindAndBodytuned() {
+    var ipareturnedData = await fetchApiData(userChannelId[1].channelId);
+
+    console.log('ipareturnedData:  ' + ipareturnedData);
+    var domContainer = $('.cotainer');
+    ipareturnedData.items.forEach((el) => {
+        console.log(el);
+        var videoId = el.id.videoId;
+        var channelId = el.snippet.channelId;
+        var channelTitle = el.snippet.title;
+        var description = el.snippet.description;
+
+        // console.log(`videoId: ${videoId} channelId: ${channelId} channelTitle: ${channelTitle} description: ${description}`)
+        // console.log(youtube + "v=" + videoId + "&ab_channel=" + channelId);
+
+        var videos = `<iframe width="420" height="315" src='${youtube}${videoId}'></iframe>
+                    <p><h2>${channelTitle}</h2></p>
+                    <p>${description}</p>`
+        domContainer.append(videos);
+    });
+}
+keepMindAndBodytuned()
+
+async function BuildAdefinedBody() {
+    var ipareturnedData = await fetchApiData(userChannelId[2].channelId);
+    console.log('BuildAdefinedBody function:  ' + ipareturnedData);
+    var domContainer = $('.cotainer');
+    ipareturnedData.items.forEach((el) => {
+        console.log(el);
+        var videoId = el.id.videoId;
+        var channelId = el.snippet.channelId;
+        var channelTitle = el.snippet.title;
+        var description = el.snippet.description;
+
+        // console.log(`videoId: ${videoId} channelId: ${channelId} channelTitle: ${channelTitle} description: ${description}`)
+        // console.log(youtube + "v=" + videoId + "&ab_channel=" + channelId);
+
+        var videos = `<iframe width="420" height="315" src='${youtube}${videoId}'></iframe>
+                <p><h2>${channelTitle}</h2></p>
+                <p>${description}</p>`
+        domContainer.append(videos);
+    });
+}
+
+BuildAdefinedBody();
+
+
+
+
